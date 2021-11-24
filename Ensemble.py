9ca5b667c51412829d809f4b6aa6d2c2d6b858c9
vikashV3 <58329489+vikashV3@users.noreply.github.com>
2021-11-24 10:14:30 +0530

    Create Ensemble.py

---
+++ Ensemble.py
@@ -0,0 +1,30 @@
+#Ensemble Technique
+from sklearn.ensemble import VotingClassifier
+Ens = VotingClassifier( estimators= [('SVM',svc),('NB',nab),('Log',log),('raf',raf)], voting = 'hard')
+Ens= Ens.fit(X_train , y_train)
+Ens
+
+#Accuracy
+y_pred1 = Ens.predict(X_test)
+en=Ens.score(X_test, y_test)
+print('Accuracy score= {:.2f}'.format(Ens.score(X_test, y_test)))
+
+#Confusion Matrix
+
+from sklearn.metrics import classification_report, confusion_matrix
+from mlxtend.plotting import plot_confusion_matrix
+
+
+print('\n')
+print("confusion matrix")
+print('\n')
+CR=confusion_matrix(y_test, y_pred1)
+print(CR)
+print('\n')
+
+fig, ax = plot_confusion_matrix(conf_mat=CR,figsize=(10, 10),
+                                show_absolute=True,
+                                show_normed=True,
+                                colorbar=True)
+plt.show()
+
