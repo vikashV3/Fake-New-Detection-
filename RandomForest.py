6f472777caffb6a5f41a47299b6bb362e9a353a6
vikashV3 <58329489+vikashV3@users.noreply.github.com>
2021-11-24 09:28:49 +0530

    Create RandomForest.py

---
+++ RandomForest.py
@@ -0,0 +1,35 @@
+#RANDOM FOREST ALGORITHM
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
+
+raf=RandomForestClassifier(min_samples_leaf=20, min_samples_split=20,random_state=100)
+raf= raf.fit(X_train , y_train)
+raf
+
+#Accuracy 
+y_pred1 = raf.predict(X_test)
+rf=raf.score(X_test, y_test)
+print('Accuracy score= {:.2f}'.format(raf.score(X_test, y_test)))
+
+
+#Confusion Matrix
+from sklearn.metrics import classification_report, confusion_matrix
+from mlxtend.plotting import plot_confusion_matrix
+
+
+print('\n')
+print("confusion matrix")
+print('\n')
+CR=confusion_matrix(y_test, y_pred1)
+print(CR)
+print('\n')
+
+fig, ax = plot_confusion_matrix(conf_mat=CR,figsize=(10, 10),
+                                show_absolute=True,
+                                show_normed=True,
+                                colorbar=True)
+plt.show()
+
